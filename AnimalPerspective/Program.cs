64b4cde1c8253b690f833354c8c92ea1448f4564
Pninja12 <74876860+Pninja12@users.noreply.github.com>
2023-05-11 19:32:33 +0100

    Sons dos animais adicionados, incluindo o morcego e a abelha, e a interface foi inicializada

---
+++ AnimalPerspective/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace AnimalPerspective
+{
+    public class Animal
+    {
+        public virtual string Sound()
+        {
+            return "Sound will be ";
+        }
+    }
+    public class Cat : Animal
+    {
+        public override string Sound()
+        {
+            return base.Sound() + "Miau";
+        }
+    }
+    public class Dog : Animal
+    {
+        public override string Sound()
+        {
+            return base.Sound() + "Woof!";
+        }
+    }
+    public class Bat : Animal
+    {
+        public override string Sound()
+        {
+            return base.Sound() + "Chirp";
+        }
+    }
+    public class Bee : Animal
+    {
+        public override string Sound()
+        {
+            return base.Sound() + "Bzzz";
+        }
+    }
+
+    public interface IMammal
+    {
+        int NumberOfNipples {get; }
+        void Burn ( ) ;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
