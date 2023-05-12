d6c27868c83e9657950fb83c50d6d48d418bc3ba
Pninja12 <74876860+Pninja12@users.noreply.github.com>
2023-05-12 21:18:42 +0100

    Exercício 2 acabado

--- AnimalPerspective/Program.cs
+++ AnimalPerspective/Program.cs
@@ -6,7 +6,7 @@ namespace AnimalPerspective
     {
         public virtual string Sound()
         {
-            return "Sound will be ";
+            return ": ";
         }
     }
     public class Cat : Animal, IMammal
@@ -59,9 +59,65 @@ namespace AnimalPerspective
     }
     class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Animal[] animalsarray = new Animal[10];
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                animalsarray[i] = random.Next(0, 3) switch
+                {
+                    0 => new Bat(),
+                    1 => new Bee(),
+                    2 => new Cat(),
+                    3 => new Dog()
+                };
+            }
+
+            foreach (Animal animal in animalsarray)
+            {
+                Console.WriteLine("--------------------------------");
+                Console.WriteLine();
+                Console.Write($"{animal.GetType().Name} says");
+                Console.WriteLine($"{animal.Sound()}");
+                if (animal is IMammal)
+                {
+                    Console.WriteLine($"Nº of Nipples = {(animal as IMammal).NumberOfNipples}");
+                }
+                if (animal is ICanFly)
+                {
+                    Console.WriteLine($"Nº of Wings = {(animal as ICanFly).NumberOfWings}");
+                }
+                Console.WriteLine();
+                Console.WriteLine("--------------------------------");
+            }
+
+            /* const int n = 10;
+            Animal[] animals = new Animal[n];
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                animals[i] = rnd.Next(4) switch
+                {
+                    0 => new Bat(),
+                    1 => new Bee(),
+                    2 => new Cat(),
+                    3 => new Dog(),
+                    _ => throw new ArgumentOutOfRangeException("Unknown animal!")
+                };
+            }
+
+            foreach (Animal a in animals)
+            {
+                Console.Write($"{a.GetType().Name} says ");
+                Console.Write(a.Sound());
+                if (a is IMammal)
+                    Console.Write($", No.Nipples = {(a as IMammal).NumberOfNipples}");
+                if (a is ICanFly)
+                    Console.Write($", No.Wings = {(a as ICanFly).NumberOfWings}");
+                Console.WriteLine();
+            } */
         }
     }
 }
